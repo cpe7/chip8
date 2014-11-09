b4b5f0ef388635666b04b6a30c794dd3a75332f4
cpe7 <car12384@live.com>
2014-11-09 03:11:57 -0500

    Begin parsing instruction set for dissassembly.
    11/9/14: Simple parsing of instruction 2 byte words.

--- chip8emu/chip8emu/chip8emu.cpp
+++ chip8emu/chip8emu/chip8emu.cpp
@@ -1,13 +1,16 @@
 // chip8emu.cpp : Defines the entry point for the console application.
 // Author        : Lauren Rush
-// References    : [1] http://www.cplusplus.com/doc/tutorial/files/
-//
+// References    : [1] file i/o: http://www.cplusplus.com/doc/tutorial/files/
+//                 [2] two bytes at a time: http://www.cplusplus.com/forum/beginner/48160/
+//                 [3] Chip-8 Instruction set: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.0
 
 #include "stdafx.h"
 
-
 int _tmain(int argc, _TCHAR* argv[])
 {
+	unsigned int i = 0;
+	char msb, lsb;
+	short inst = 0;
 	ifstream myROM;
 
 	// (1) Open binary file [1]
@@ -17,11 +20,205 @@ int _tmain(int argc, _TCHAR* argv[])
 	if (myROM.is_open())
 	{
 		// Until the end of the file, grab bytes...
-		while (!myROM.eof())
-		{
-			// Print hexidecimal value to console...
-			cout << hex << myROM.get() << " ";
-		}
+        do {
+			// Read two bytes at a time to [2]
+			msb = myROM.get();
+			lsb = myROM.get();
+
+			inst = ((msb << 8) | lsb); // Reconstitute instruction, 2 bytes [2]
+
+			// Print hexidecimal value to console... [2]
+			cout << i++ << ": " << hex << inst << " - ";
+
+			//***********************
+			// Parse Instructions [3]
+			//***********************
+			// (1) 0nnn - SYS addr
+			//	Jump to a machine code routine at nnn.
+			//	This instruction is only used on the old computers on which 
+			//	Chip - 8 was originally implemented.  It is ignored by modern interpreters.
+
+			//***********************
+			// (2) 00E0 - CLS
+			//	Clear the display.
+			if(inst == 0x00E0)
+				cout << "Clear the display.\n";
+
+			//***********************
+			// (3) 00EE - RET
+			//	Return from a subroutine.
+			//	The interpreter sets the program counter to the address at
+			//	the top of the stack, then subtracts 1 from the stack pointer.
+			if(inst == 0x00EE)
+				cout << "Return from a subroutine.\n";
+			
+			//***********************
+			// (4) 1nnn - JP addr
+			//	Jump to location nnn.
+			//	The interpreter sets the program counter to nnn.
+			if((inst & 0xF000) == 0x1000)
+				cout << "Jump to location " << (0x0FFF & inst);
+
+			//***********************
+			// (5) 2nnn - CALL addr
+			//	Call subroutine at nnn.
+			//	The interpreter increments the stack pointer, then puts the current 
+			//  PC on the top of the stack.The PC is then set to nnn.
+			if((inst & 0xF000) == 0x2000)
+				  cout << "Call subroutine at " << (0x0FFF & inst);
+
+			//***********************
+			// (6) 3xkk - SE Vx, byte
+			//	Skip next instruction if Vx = kk.
+			//	The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+
+			//***********************			
+			// (7) 4xkk - SNE Vx, byte
+			//	Skip next instruction if Vx != kk.
+			//	The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+
+			//***********************
+			// (8) 5xy0 - SE Vx, Vy
+			//	Skip next instruction if Vx = Vy.
+			//	The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
+
+			//***********************
+			// (9) 6xkk - LD Vx, byte
+			//	Set Vx = kk.
+			//	The interpreter puts the value kk into register Vx.
+
+			//***********************
+			// (10)	7xkk - ADD Vx, byte
+			//	Set Vx = Vx + kk.
+			//	Adds the value kk to the value of register Vx, then stores the result in Vx.
+
+			//***********************
+			// (11)	8xy0 - LD Vx, Vy
+			//	Set Vx = Vy.
+			//	Stores the value of register Vy in register Vx.
+
+			//***********************
+			// (12)	8xy1 - OR Vx, Vy
+			//	Set Vx = Vx OR Vy.
+			//	Performs a bitwise OR on the values of Vx and Vy, then stores the result in Vx.A bitwise OR compares the corrseponding bits from two values, and if either bit is 1, then the same bit in the result is also 1. Otherwise, it is 0.
+
+			//***********************
+			// (13)	8xy2 - AND Vx, Vy
+			//	Set Vx = Vx AND Vy.
+			//	Performs a bitwise AND on the values of Vx and Vy, then stores the result in Vx.A bitwise AND compares the corrseponding bits from two values, and if both bits are 1, then the same bit in the result is also 1. Otherwise, it is 0.
+
+			//***********************
+			// (14)	8xy3 - XOR Vx, Vy
+			//	Set Vx = Vx XOR Vy.
+			//	Performs a bitwise exclusive OR on the values of Vx and Vy, then stores the result in Vx.An exclusive OR compares the corrseponding bits from two values, and if the bits are not both the same, then the corresponding bit in the result is set to 1. Otherwise, it is 0.
+
+			//***********************
+			// (15)	8xy4 - ADD Vx, Vy
+			//	Set Vx = Vx + Vy, set VF = carry.
+			//	The values of Vx and Vy are added together.If the result is greater than 8 bits(i.e., > 255, ) VF is set to 1, otherwise 0. Only the lowest 8 bits of the result are kept, and stored in Vx.
+
+			//***********************
+			// (16)	8xy5 - SUB Vx, Vy
+			//	Set Vx = Vx - Vy, set VF = NOT borrow.
+			//	If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
+
+			//***********************
+			// (17)	8xy6 - SHR Vx{ , Vy }
+			//  Set Vx = Vx SHR 1.
+			//	If the least - significant bit of Vx is 1, then VF is set to 1, otherwise 0. Then Vx is divided by 2.
+
+			//***********************
+			// (18)	8xy7 - SUBN Vx, Vy
+			//	Set Vx = Vy - Vx, set VF = NOT borrow.
+			//	If Vy > Vx, then VF is set to 1, otherwise 0. Then Vx is subtracted from Vy, and the results stored in Vx.
+
+			//***********************
+			// (19)	8xyE - SHL Vx{ , Vy }
+			//  Set Vx = Vx SHL 1.
+			//	If the most - significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2.
+
+			//***********************
+			// (20)	9xy0 - SNE Vx, Vy
+			//	Skip next instruction if Vx != Vy.
+			//	The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.
+
+			//***********************
+			// (21)	Annn - LD I, addr
+			//	Set I = nnn.
+			//	The value of register I is set to nnn.
+
+			//***********************
+			// (22)	Bnnn - JP V0, addr
+			//	Jump to location nnn + V0.
+			//	The program counter is set to nnn plus the value of V0.
+
+			//***********************
+			// (23)	Cxkk - RND Vx, byte
+			//	Set Vx = random byte AND kk.
+			//	The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk.The results are stored in Vx.See instruction 8xy2 for more information on AND.
+
+			//***********************
+			// (24)	Dxyn - DRW Vx, Vy, nibble
+			//	Display n - byte sprite starting at memory location I at(Vx, Vy), set VF = collision.
+			//	The interpreter reads n bytes from memory, starting at the address stored in I.These bytes are then displayed as sprites on screen at coordinates(Vx, Vy).Sprites are XORed onto the existing screen.If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen.See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip - 8 screen and sprites.
+
+			//***********************
+			// (25) Ex9E - SKP Vx
+			//	Skip next instruction if key with the value of Vx is pressed.
+			//	Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+
+			//***********************
+			// (26) ExA1 - SKNP Vx
+			//	Skip next instruction if key with the value of Vx is not pressed.
+			//	Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+
+			//***********************
+			// (27)	Fx07 - LD Vx, DT
+			//	Set Vx = delay timer value.
+			//	The value of DT is placed into Vx.
+
+			//***********************
+			// (28)	Fx0A - LD Vx, K
+			//	Wait for a key press, store the value of the key in Vx.
+			//	All execution stops until a key is pressed, then the value of that key is stored in Vx.
+
+			//***********************
+			// (29)	Fx15 - LD DT, Vx
+			//	Set delay timer = Vx.
+			//	DT is set equal to the value of Vx.
+
+			//***********************
+			// (30)	Fx18 - LD ST, Vx
+			//	Set sound timer = Vx.
+			//	ST is set equal to the value of Vx.
+
+			//***********************
+			// (31)	Fx1E - ADD I, Vx
+			//	Set I = I + Vx.
+			//	The values of I and Vx are added, and the results are stored in I.
+
+			//***********************
+			// (32)	Fx29 - LD F, Vx
+			//	Set I = location of sprite for digit Vx.
+			//	The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx.See section 2.4, Display, for more information on the Chip - 8 hexadecimal font.
+
+			//***********************
+			// (33)	Fx33 - LD B, Vx
+			//	Store BCD representation of Vx in memory locations I, I + 1, and I + 2.
+			//	The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I + 1, and the ones digit at location I + 2.
+
+			//***********************
+			// (34)	Fx55 - LD[I], Vx
+			//	Store registers V0 through Vx in memory starting at location I.
+			//	The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+
+			//***********************
+			// (35)	Fx65 - LD Vx, [I]
+			//	Read registers V0 through Vx from memory starting at location I.
+			//	The interpreter reads values from memory starting at location I into registers V0 through Vx.
+
+			cout << endl;
+		} while (myROM.peek() != EOF);
 
 		// Close file, when done...
 		myROM.close();
--- chip8emu/chip8emu/stdafx.h
+++ chip8emu/chip8emu/stdafx.h
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 
